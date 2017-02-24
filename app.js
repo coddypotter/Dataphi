8bb03d15860f523c51a3f38e4b896619449135fa
sudeepst <sudeep@loktra.com>
2017-02-24 23:36:33 +0530

    Server Running

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.get('/', function (req, res) {
     res.status(200).json({ message: "Service Live" });
 });
 
-app.listen(3000, function () {
-    console.log('Express server listening on port 3000');
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
