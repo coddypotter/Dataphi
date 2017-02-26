e17a474111e3f5a6d1fbad1ac767e214698636a5
sudeepst <sudeep@loktra.com>
2017-02-26 13:13:51 +0530

    Added CORS

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
 var models = require('./models');
 var app = express();
 var patientRouter = require('./routes/patient');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/patient/', patientRouter);
 app.get('/', function (req, res) {
   res.status(200).json({ message: "Service Live" });
